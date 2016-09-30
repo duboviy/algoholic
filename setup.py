e366c763b38a36a5a94865a7e9ec0d9e1fc1070b
Eugene Dubovoy <eugene.dubovoy@gmail.com>
2016-09-30 15:42:35 +0300

    Changed directory structure to release it more concisely

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'algoholic',
   packages = ['algoholic'], # this must be the same as the name above
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'A sandbox for trying out algorithms.',
   author = 'Eugene Duboviy',
   author_email = 'eugene.dubovoy@gmail.com',
   url = 'https://github.com/duboviy/algoholic', # use the URL to the github repo
-  download_url = 'https://github.com/duboviy/algoholic/tarball/0.0.1', # I'll explain this in a second
+  download_url = 'https://github.com/duboviy/algoholic/tarball/0.0.2', # I'll explain this in a second
   keywords = ['algorithms', 'performance', 'testing'], # arbitrary keywords
   classifiers = [],
 )
